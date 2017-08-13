6172ee646dd5f9894a7677843466d9b330c879d9
Constantin Bugneac <cosbug@gmail.com>
2017-08-13 20:23:04 +0100

    added new sg

--- terraform/core.tf
+++ terraform/core.tf
@@ -169,3 +169,28 @@ resource "aws_security_group" "dev_sg" {
     Project = "${var.project}"
   }
 }
+
+resource "aws_security_group" "server_sg" {
+  name        = "server_sg"
+  description = "Dev server security group"
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name    = "server_sg"
+    Env     = "${var.environment}"
+    Project = "${var.project}"
+  }
+}
