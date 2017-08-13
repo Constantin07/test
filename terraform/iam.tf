493a4e6bb29654cfbcb0cf38802d9c398a48db28
Constantin Bugneac <cosbug@gmail.com>
2017-08-13 20:48:23 +0100

    max pass to remember

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,7 +3,7 @@ resource "aws_iam_account_password_policy" "custom" {
   allow_users_to_change_password = true
   max_password_age               = 180
   minimum_password_length        = 8
-  password_reuse_prevention      = true
+  password_reuse_prevention      = 3
   require_lowercase_characters   = true
   require_numbers                = true
   require_symbols                = true
