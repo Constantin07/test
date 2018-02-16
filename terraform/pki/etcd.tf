9141273d231194b04a845f39ee0acac502f6c125
Constantin Bugneac <cosbug@gmail.com>
2018-02-16 17:42:51 +0000

    added etcd cert

--- terraform/pki/etcd.tf
+++ terraform/pki/etcd.tf
@@ -30,8 +30,8 @@ resource "tls_cert_request" "etcd_cert_req" {
 resource "tls_locally_signed_cert" "etcd_cert" {
   cert_request_pem   = "${tls_cert_request.etcd_cert_req.cert_request_pem}"
   ca_key_algorithm   = "${tls_self_signed_cert.ca.ca_key_algorithm}"
-  ca_private_key_pem = "${tls_self_signed_cert.ca.ca_private_key_pem}"
-  ca_cert_pem        = "${tls_self_signed_cert.ca.ca_cert_pem}"
+  ca_private_key_pem = "${file("${path.module}/files/ca-priv-key.pem")}"
+  ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
 
   is_ca_certificate     = false
   validity_period_hours = 87600
