427a3643d3209833bb01650b61473c2127e59b24
Constantin Bugneac <cosbug@gmail.com>
2018-02-26 23:31:30 +0000

    updated etcd role

--- terraform/pki/etcd.tf
+++ terraform/pki/etcd.tf
@@ -1,35 +1,32 @@
-/* etcd server certificate */
+/* etcd-1 server certificate */
 
-resource "tls_private_key" "etcd_server_priv_key" {
+resource "tls_private_key" "etcd-1_server_priv_key" {
   algorithm = "RSA"
   rsa_bits  = "2048"
 }
 
-resource "tls_cert_request" "etcd_server_cert_req" {
-  key_algorithm   = "${tls_private_key.etcd_server_priv_key.algorithm}"
-  private_key_pem = "${tls_private_key.etcd_server_priv_key.private_key_pem}"
+resource "tls_cert_request" "etcd-1_server_cert_req" {
+  key_algorithm   = "${tls_private_key.etcd-1_server_priv_key.algorithm}"
+  private_key_pem = "${tls_private_key.etcd-1_server_priv_key.private_key_pem}"
 
   subject {
-    common_name  = "etcd server"
+    common_name  = "etcd-1.internal"
     organization = "etcd"
   }
 
   dns_names = [
     "etcd-1.internal",
-    "etcd-2.internal",
-    "etcd-3.internal",
-    "etcd.internal",
     "localhost",
   ]
 
   ip_addresses = [
-    "127.0.0.1",
     "10.0.2.24",
+    "127.0.0.1",
   ]
 }
 
-resource "tls_locally_signed_cert" "etcd_server_cert" {
-  cert_request_pem   = "${tls_cert_request.etcd_server_cert_req.cert_request_pem}"
+resource "tls_locally_signed_cert" "etcd-1_server_cert" {
+  cert_request_pem   = "${tls_cert_request.etcd-1_server_cert_req.cert_request_pem}"
   ca_key_algorithm   = "${local.ca_key_algorithm}"
   ca_private_key_pem = "${local.ca_private_key_pem}"
   ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
@@ -41,6 +38,99 @@ resource "tls_locally_signed_cert" "etcd_server_cert" {
   allowed_uses = [
     "digital_signature",
     "server_auth",
+    "client_auth",
+    "data_encipherment",
+    "key_encipherment",
+  ]
+}
+
+/* etcd-2 server certificate */
+
+resource "tls_private_key" "etcd-2_server_priv_key" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "etcd-2_server_cert_req" {
+  key_algorithm   = "${tls_private_key.etcd-2_server_priv_key.algorithm}"
+  private_key_pem = "${tls_private_key.etcd-2_server_priv_key.private_key_pem}"
+
+  subject {
+    common_name  = "etcd-2.internal"
+    organization = "etcd"
+  }
+
+  dns_names = [
+    "etcd-2.internal",
+    "localhost",
+  ]
+
+  ip_addresses = [
+    "10.0.2.25",
+    "127.0.0.1",
+  ]
+}
+
+resource "tls_locally_signed_cert" "etcd-2_server_cert" {
+  cert_request_pem   = "${tls_cert_request.etcd-2_server_cert_req.cert_request_pem}"
+  ca_key_algorithm   = "${local.ca_key_algorithm}"
+  ca_private_key_pem = "${local.ca_private_key_pem}"
+  ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
+
+  is_ca_certificate     = false
+  validity_period_hours = 87600
+  early_renewal_hours   = 3160
+
+  allowed_uses = [
+    "digital_signature",
+    "server_auth",
+    "client_auth",
+    "data_encipherment",
+    "key_encipherment",
+  ]
+}
+
+/* etcd-3 server certificate */
+
+resource "tls_private_key" "etcd-3_server_priv_key" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "etcd-3_server_cert_req" {
+  key_algorithm   = "${tls_private_key.etcd-3_server_priv_key.algorithm}"
+  private_key_pem = "${tls_private_key.etcd-3_server_priv_key.private_key_pem}"
+
+  subject {
+    common_name  = "etcd-3.internal"
+    organization = "etcd"
+  }
+
+  dns_names = [
+    "etcd-3.internal",
+    "localhost",
+  ]
+
+  ip_addresses = [
+    "10.0.2.26",
+    "127.0.0.1",
+  ]
+}
+
+resource "tls_locally_signed_cert" "etcd-3_server_cert" {
+  cert_request_pem   = "${tls_cert_request.etcd-3_server_cert_req.cert_request_pem}"
+  ca_key_algorithm   = "${local.ca_key_algorithm}"
+  ca_private_key_pem = "${local.ca_private_key_pem}"
+  ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
+
+  is_ca_certificate     = false
+  validity_period_hours = 87600
+  early_renewal_hours   = 3160
+
+  allowed_uses = [
+    "digital_signature",
+    "server_auth",
+    "client_auth",
     "data_encipherment",
     "key_encipherment",
   ]
@@ -81,52 +171,45 @@ resource "tls_locally_signed_cert" "etcd_client_cert" {
   ]
 }
 
-/* etcd peer certificate */
-
-resource "tls_private_key" "etcd_peer_priv_key" {
-  algorithm = "RSA"
-  rsa_bits  = "2048"
+output "etcd-1_server_cert_pem" {
+  description = "Etcd-1 server certificate in PEM format"
+  value       = "${tls_locally_signed_cert.etcd-1_server_cert.cert_pem}"
 }
 
-resource "tls_cert_request" "etcd_peer_cert_req" {
-  key_algorithm   = "${tls_private_key.etcd_peer_priv_key.algorithm}"
-  private_key_pem = "${tls_private_key.etcd_peer_priv_key.private_key_pem}"
+output "etcd-1_server_cert_validity" {
+  description = "Etcd-1 server certificate validity timeframe"
 
-  subject {
-    common_name  = "etcd peer"
-    organization = "etcd"
+  value = {
+    start = "${tls_locally_signed_cert.etcd-1_server_cert.validity_start_time}"
+    end   = "${tls_locally_signed_cert.etcd-1_server_cert.validity_end_time}"
   }
 }
 
-resource "tls_locally_signed_cert" "etcd_peer_cert" {
-  cert_request_pem   = "${tls_cert_request.etcd_peer_cert_req.cert_request_pem}"
-  ca_key_algorithm   = "${local.ca_key_algorithm}"
-  ca_private_key_pem = "${local.ca_private_key_pem}"
-  ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
-
-  is_ca_certificate     = false
-  validity_period_hours = 87600
-  early_renewal_hours   = 3160
-
-  allowed_uses = [
-    "digital_signature",
-    "client_auth",
-    "data_encipherment",
-    "key_encipherment",
-  ]
+output "etcd-2_server_cert_pem" {
+  description = "Etcd-2 server certificate in PEM format"
+  value       = "${tls_locally_signed_cert.etcd-2_server_cert.cert_pem}"
 }
 
-output "etcd_server_cert_pem" {
-  description = "Etcd server certificate in PEM format"
-  value       = "${tls_locally_signed_cert.etcd_server_cert.cert_pem}"
-}
-
-output "etcd_server_cert_validity" {
-  description = "Etcd server certificate validity timeframe"
+output "etcd-2_server_cert_validity" {
+  description = "Etcd-2 server certificate validity timeframe"
 
   value = {
-    start = "${tls_locally_signed_cert.etcd_server_cert.validity_start_time}"
-    end   = "${tls_locally_signed_cert.etcd_server_cert.validity_end_time}"
+    start = "${tls_locally_signed_cert.etcd-2_server_cert.validity_start_time}"
+    end   = "${tls_locally_signed_cert.etcd-2_server_cert.validity_end_time}"
+  }
+}
+
+output "etcd-3_server_cert_pem" {
+  description = "Etcd-3 server certificate in PEM format"
+  value       = "${tls_locally_signed_cert.etcd-3_server_cert.cert_pem}"
+}
+
+output "etcd-3_server_cert_validity" {
+  description = "Etcd-3 server certificate validity timeframe"
+
+  value = {
+    start = "${tls_locally_signed_cert.etcd-3_server_cert.validity_start_time}"
+    end   = "${tls_locally_signed_cert.etcd-3_server_cert.validity_end_time}"
   }
 }
 
@@ -141,19 +224,5 @@ output "etcd_client_cert_validity" {
   value = {
     start = "${tls_locally_signed_cert.etcd_client_cert.validity_start_time}"
     end   = "${tls_locally_signed_cert.etcd_client_cert.validity_end_time}"
-  }
-}
-
-output "etcd_peer_cert_pem" {
-  description = "Etcd peer certificate in PEM format"
-  value       = "${tls_locally_signed_cert.etcd_peer_cert.cert_pem}"
-}
-
-output "etcd_peer_cert_validity" {
-  description = "Etcd peer certificate validity timeframe"
-
-  value = {
-    start = "${tls_locally_signed_cert.etcd_peer_cert.validity_start_time}"
-    end   = "${tls_locally_signed_cert.etcd_peer_cert.validity_end_time}"
   }
 }
