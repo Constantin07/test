41a3212f922394c86ca010727d43356ff71a211c
Constantin Bugneac <cosbug@gmail.com>
2018-09-17 22:47:21 +0100

    update account number

--- terraform/env_test/terraform.tf
+++ terraform/env_test/terraform.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "costea-states"
+    bucket         = "costea2-states"
     key            = "test/core.tfstate"
-    dynamodb_table = "costea-states"
+    //dynamodb_table = "costea-states"
     encrypt        = true
     region         = "eu-west-1"
 
-    role_arn     = "arn:aws:iam::532814210204:role/TerraformAdmin"
+    role_arn     = "arn:aws:iam::705505438149:role/TerraformAdmin"
     session_name = "Terraform"
   }
 }
@@ -15,10 +15,10 @@ provider "aws" {
   region  = "${var.region}"
   version = ">= 1.28.0"
 
-  allowed_account_ids = ["532814210204"]
+  allowed_account_ids = ["705505438149"]
 
   assume_role {
-    role_arn     = "arn:aws:iam::532814210204:role/TerraformAdmin"
+    role_arn     = "arn:aws:iam::705505438149:role/TerraformAdmin"
     session_name = "Terraform"
   }
 }
