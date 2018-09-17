6d906e6a2f4ef57ea10a5558bb9ae38edcc26e75
Constantin Bugneac <cosbug@gmail.com>
2018-09-17 22:43:01 +0100

    updated account name

--- terraform/pki/terraform.tf
+++ terraform/pki/terraform.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 0.11.7"
+  required_version = ">= 0.11.8"
 
   backend "s3" {
-    bucket         = "costea-states"
+    bucket         = "costea2-states"
     key            = "terraform/pki.tfstate"
-    dynamodb_table = "costea-states"
+    //dynamodb_table = "costea-states"
     encrypt        = true
     region         = "eu-west-1"
 
-    role_arn     = "arn:aws:iam::532814210204:role/TerraformAdmin"
+    role_arn     = "arn:aws:iam::705505438149:role/TerraformAdmin"
     session_name = "Terraform"
   }
 }
