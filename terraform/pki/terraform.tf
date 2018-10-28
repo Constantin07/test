85359c13780ea2ef3d810c0f5647618083f25ecb
Constantin Bugneac <cosbug@gmail.com>
2018-10-28 22:39:40 +0000

    updated pki plan

--- terraform/pki/terraform.tf
+++ terraform/pki/terraform.tf
@@ -1,18 +1,15 @@
 terraform {
-  required_version = ">= 0.11.8"
+  required_version = ">= 0.11.10"
 
   backend "s3" {
-    bucket         = "costea2-states"
-    key            = "terraform/pki.tfstate"
+    bucket = "costea2-states"
+    key    = "terraform/pki.tfstate"
+
     //dynamodb_table = "costea-states"
-    encrypt        = true
-    region         = "eu-west-1"
+    encrypt = true
+    region  = "eu-west-1"
 
     role_arn     = "arn:aws:iam::705505438149:role/TerraformAdmin"
     session_name = "Terraform"
   }
-}
-
-provider "tls" {
-  version = ">= 1.2.0"
 }
