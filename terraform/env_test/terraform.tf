5c59892d4b978e96f4322c447f01384e92c5cbe9
Constantin Bugneac <cosbug@gmail.com>
2018-10-28 22:44:31 +0000

    updated env_test plan

--- terraform/env_test/terraform.tf
+++ terraform/env_test/terraform.tf
@@ -1,23 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "costea2-states"
-    key            = "test/core.tfstate"
+    bucket = "costea2-states"
+    key    = "test/core.tfstate"
+
     //dynamodb_table = "costea-states"
-    encrypt        = true
-    region         = "eu-west-1"
+    encrypt = true
+    region  = "eu-west-1"
 
-    role_arn     = "arn:aws:iam::705505438149:role/TerraformAdmin"
-    session_name = "Terraform"
-  }
-}
-
-provider "aws" {
-  region  = "${var.region}"
-  version = ">= 1.36.0"
-
-  allowed_account_ids = ["705505438149"]
-
-  assume_role {
     role_arn     = "arn:aws:iam::705505438149:role/TerraformAdmin"
     session_name = "Terraform"
   }
