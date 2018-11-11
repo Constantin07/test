6023a35af8e255b3feb71b2ea90f7a7cc863d144
Constantin Bugneac <cosbug@gmail.com>
2018-11-11 20:35:27 +0000

    updated etcd certs

--- terraform/pki/etcd.tf
+++ terraform/pki/etcd.tf
@@ -141,23 +141,23 @@ resource "tls_locally_signed_cert" "etcd-3_server_cert" {
 
 /* etcd client certificate */
 
-resource "tls_private_key" "etcd_client_priv_key" {
+resource "tls_private_key" "etcd-client_priv_key" {
   algorithm = "RSA"
   rsa_bits  = "2048"
 }
 
-resource "tls_cert_request" "etcd_client_cert_req" {
-  key_algorithm   = "${tls_private_key.etcd_client_priv_key.algorithm}"
-  private_key_pem = "${tls_private_key.etcd_client_priv_key.private_key_pem}"
+resource "tls_cert_request" "etcd-client_cert_req" {
+  key_algorithm   = "${tls_private_key.etcd-client_priv_key.algorithm}"
+  private_key_pem = "${tls_private_key.etcd-client_priv_key.private_key_pem}"
 
   subject {
-    common_name  = "etcd client"
+    common_name  = "etcd-client"
     organization = "${var.organisation}"
   }
 }
 
-resource "tls_locally_signed_cert" "etcd_client_cert" {
-  cert_request_pem   = "${tls_cert_request.etcd_client_cert_req.cert_request_pem}"
+resource "tls_locally_signed_cert" "etcd-client_cert" {
+  cert_request_pem   = "${tls_cert_request.etcd-client_cert_req.cert_request_pem}"
   ca_key_algorithm   = "${local.ca_key_algorithm}"
   ca_private_key_pem = "${local.ca_private_key_pem}"
   ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
@@ -172,60 +172,4 @@ resource "tls_locally_signed_cert" "etcd_client_cert" {
     "data_encipherment",
     "key_encipherment",
   ]
-}
-
-output "etcd-1_server_cert_pem" {
-  description = "Etcd-1 server certificate in PEM format"
-  value       = "${tls_locally_signed_cert.etcd-1_server_cert.cert_pem}"
-}
-
-output "etcd-1_server_cert_validity" {
-  description = "Etcd-1 server certificate validity timeframe"
-
-  value = {
-    start = "${tls_locally_signed_cert.etcd-1_server_cert.validity_start_time}"
-    end   = "${tls_locally_signed_cert.etcd-1_server_cert.validity_end_time}"
-  }
-}
-
-output "etcd-2_server_cert_pem" {
-  description = "Etcd-2 server certificate in PEM format"
-  value       = "${tls_locally_signed_cert.etcd-2_server_cert.cert_pem}"
-}
-
-output "etcd-2_server_cert_validity" {
-  description = "Etcd-2 server certificate validity timeframe"
-
-  value = {
-    start = "${tls_locally_signed_cert.etcd-2_server_cert.validity_start_time}"
-    end   = "${tls_locally_signed_cert.etcd-2_server_cert.validity_end_time}"
-  }
-}
-
-output "etcd-3_server_cert_pem" {
-  description = "Etcd-3 server certificate in PEM format"
-  value       = "${tls_locally_signed_cert.etcd-3_server_cert.cert_pem}"
-}
-
-output "etcd-3_server_cert_validity" {
-  description = "Etcd-3 server certificate validity timeframe"
-
-  value = {
-    start = "${tls_locally_signed_cert.etcd-3_server_cert.validity_start_time}"
-    end   = "${tls_locally_signed_cert.etcd-3_server_cert.validity_end_time}"
-  }
-}
-
-output "etcd_client_cert_pem" {
-  description = "Etcd client certificate in PEM format"
-  value       = "${tls_locally_signed_cert.etcd_client_cert.cert_pem}"
-}
-
-output "etcd_client_cert_validity" {
-  description = "Etcd client certificate validity timeframe"
-
-  value = {
-    start = "${tls_locally_signed_cert.etcd_client_cert.validity_start_time}"
-    end   = "${tls_locally_signed_cert.etcd_client_cert.validity_end_time}"
-  }
 }
