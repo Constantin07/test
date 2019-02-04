56bb89496b373602364366435d9bbbc4ba22434c
Constantin Bugneac <cosbug@gmail.com>
2019-02-04 22:00:23 +0000

    update TF groovy

--- Jenkins/terraform.groovy
+++ Jenkins/terraform.groovy
@@ -2,131 +2,126 @@
 
 // Get comment of last commit
 def get_comment() {
-    def f = 'last_commit.txt'
-    def status = sh(returnStatus: true, script: "git log -1 --pretty=%B > ${f}")
-    if (status != 0) {
-        currentBuild.result = 'FAILED'
-        error "Failed to read last commit's comment"
-    } else {
-        return readFile(f).trim()
-    }
-    sh "rm ${f}"
+  def f = 'last_commit.txt'
+  def status = sh(returnStatus: true, script: "git log -1 --pretty=%B > ${f}")
+  if (status != 0) {
+    currentBuild.result = 'FAILED'
+    error "Failed to read last commit's comment"
+  } else {
+    return readFile(f).trim()
+  }
+  sh "rm ${f}"
 }
 
 def build(nodeName = '', directory = '.') {
 
-    // Global state
-    def needUpdate = false
-    def apply      = false
-    def comment    = ''
+  // Global state
+  def needUpdate = false
+  def apply      = false
+  def comment    = ''
 
-    node(nodeName) {
+  node(nodeName) {
 
-        properties(
-            [
-                durabilityHint('MAX_SURVIVABILITY'),
-                buildDiscarder(logRotator(artifactDaysToKeepStr: '', numToKeepStr: '30')),
-                pipelineTriggers([githubPush(), pollSCM("TZ=Europe/London\nH/2 * * * *")]),
-                // Allow only one job at a time
-                //disableConcurrentBuilds(),
-            ]
-        )
+    properties([
+      durabilityHint('MAX_SURVIVABILITY'),
+      buildDiscarder(logRotator(artifactDaysToKeepStr: '', numToKeepStr: '30')),
+      pipelineTriggers([githubPush(), pollSCM("TZ=Europe/London\nH/2 * * * *")]),
+      // Allow only one job at a time
+      //disableConcurrentBuilds(),
+    ])
 
-        // Set path to terraform
-        //env.PATH = "/usr/local/bin:${env.PATH}"
-        def tfHome = tool(name: 'Terraform', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool')
-        env.PATH = "${tfHome}:${env.PATH}"
+    // Set path to terraform
+    //env.PATH = "/usr/local/bin:${env.PATH}"
+    def tfHome = tool(name: 'Terraform', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool')
+    env.PATH = "${tfHome}:${env.PATH}"
 
-        ansiColor('xterm') {
+    ansiColor('xterm') {
 
-            stage('Checkout') {
-                checkout(scm)
+      stage('Checkout') {
+        checkout(scm)
 
-                // Add comment to build description
-                comment = get_comment()
-                currentBuild.description = comment
+        // Add comment to build description
+        comment = get_comment()
+        currentBuild.description = comment
+      }
+
+      milestone label: 'Checkout'
+
+      stage('Unlock secrets'){
+        sh 'git crypt unlock'
+      }
+
+      milestone label: 'Unlock secrets'
+
+      dir(path: directory) {
+
+        // Terraform AWS credentials wrapper
+        withCredentials([
+          [
+            $class: 'AmazonWebServicesCredentialsBinding',
+            credentialsId: 'Amazon Credentials',
+            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+          ]
+        ])
+        {
+          stage("Validate") {
+            println "Print terraform version"
+            sh 'terraform --version'
+
+            println "Remove the .terraform directory"
+            dir('.terraform') {
+              deleteDir()
             }
 
-            milestone label: 'Checkout'
+            println "Ensure we always start from a clean state"
+            sh '''
+              rm -f plan.out
+              rm -f terraform.tfstate.backup
+            '''
 
-            stage('Unlock secrets'){
-                sh 'git crypt unlock'
+            println "Initialise configuration"
+            retry(2) {
+              echo 'Initialize S3 backend'
+              sh 'terraform init -get=true -upgrade=true -verify-plugins=true'
             }
 
-            milestone label: 'Unlock secrets'
+            println "Syntax validation"
+            sh 'terraform validate'
+          }
 
-            dir(path: directory) {
+          milestone label: 'Validate'
 
-                // Terraform AWS credentials wrapper
-                withCredentials([
-                    [
-                        $class: 'AmazonWebServicesCredentialsBinding',
-                        credentialsId: 'Amazon Credentials',
-                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                    ]
-                ])
-                {
-                    timestamps {
-                        stage("Validate") {
-                            println "Print terraform version"
-                            sh 'terraform --version'
-
-                            println "Remove the .terraform directory"
-                            dir('.terraform') {
-                                deleteDir()
-                            }
-
-                            println "Ensure we always start from a clean state"
-                            sh '''
-                                rm -f plan.out
-                                rm -f terraform.tfstate.backup
-                            '''
-
-                            println "Initialise configuration"
-                            retry(2) {
-                                echo 'Initialize S3 backend'
-                                sh 'terraform init -get=true -upgrade=true -verify-plugins=true'
-                            }
-
-                            println "Syntax validation"
-                            sh 'terraform validate'
-                        }
-                    }
-
-                    milestone label: 'Validate'
-
-                    timestamps {
-                        stage(name: 'Plan', concurency: 1) {
-                            def exitCode = sh(script: "terraform plan -out=plan.out -detailed-exitcode", returnStatus: true)
-                            echo "Terraform plan exit code: ${exitCode}"
-                            switch (exitCode) {
-                                case 0:
-                                    echo 'No changes to apply.'
-                                    currentBuild.result = 'SUCCESS'
-                                    break
-                                case 1:
-                                    echo 'Plan Failed.'
-                                    currentBuild.result = 'FAILURE'
-                                    break
-                                case 2:
-                                    echo 'Plan Awaiting Approval.'
-                                    needUpdate = true
-                                    stash(name: 'plan', includes: 'plan.out')
-                                    break
-                            }
-                        }
-                    }
-
-                    milestone label: 'Plan'
-
-                    if (needUpdate) {
-                        println "Send a notification here"
-                    }
-                }
+          stage(name: 'Plan', concurency: 1) {
+            def exitCode = sh(script: "terraform plan -out=plan.out -detailed-exitcode", returnStatus: true)
+            echo "Terraform plan exit code: ${exitCode}"
+            switch (exitCode) {
+              case 0:
+                echo 'No changes to apply.'
+                currentBuild.result = 'SUCCESS'
+                break
+              case 1:
+                echo 'Plan Failed.'
+                currentBuild.result = 'FAILURE'
+                break
+              case 2:
+                echo 'Plan Awaiting Approval.'
+                needUpdate = true
+                stash(name: 'plan', includes: 'plan.out')
+                break
             }
+          }
+
+          milestone label: 'Plan'
+
+          if (needUpdate) {
+            println "Send a notification here"
+          }
+
         }
+      }
     }
+  }
 
     if (needUpdate) {
         try {
@@ -179,7 +174,7 @@ def build(nodeName = '', directory = '.') {
 
         milestone label: 'Done'
 
-    }
+  }
 }
 
 return this;
