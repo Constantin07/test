cc4d7a4234514b50e375427c8cd7b44ea2f26194
Constantin Bugneac <cosbug@gmail.com>
2019-02-16 22:39:58 +0000

    update

--- Jenkins/global-shared-library/cleanupDocker.groovy
+++ Jenkins/global-shared-library/cleanupDocker.groovy
@@ -12,17 +12,15 @@ def call(String nodeName='', keep_images=1) {
 
         def image = docker.image(docker_image)
         image.pull()
-
-        sh """
-          docker run --rm --privileged \
-          --name docker-gc \
-          -e MINIMUM_IMAGES_TO_SAVE=${keep_images} \
-          -e FORCE_IMAGE_REMOVAL=1 \
-          -e GRACE_PERIOD_SECONDS=86400 \
-          -v /var/run/docker.sock:/var/run/docker.sock \
-          -v /etc:/etc:ro \
-          ${docker_image}
-        """
+        image.withRun("--privileged " +
+                      "--name docker-gc " +
+                      "-e MINIMUM_IMAGES_TO_SAVE=${keep_images} " +
+                      "-e FORCE_IMAGE_REMOVAL=1 " +
+                      "-e GRACE_PERIOD_SECONDS=86400 " +
+                      "-v /var/run/docker.sock:/var/run/docker.sock " +
+                      "-v /etc:/etc:ro"){
+                        /* do nothing */
+                      }
 
         sh 'docker ps --filter "status=exited" -q | xargs -r docker rm'
         sh 'docker volume ls --filter "dangling=true" -q | xargs -r docker volume rm'
