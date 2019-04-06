ad230c19bbcf5c0b6348a56ec783857160578786
Constantin Bugneac <cosbug@gmail.com>
2019-04-06 17:05:53 +0100

    added vault TF config

---
+++ terraform/vault/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "costea2-states"
+    key    = "terraform/vault.tfstate"
+
+    dynamodb_table = "costea2-states"
+    encrypt        = true
+    region         = "eu-west-1"
+
+    role_arn     = "arn:aws:iam::705505438149:role/TerraformAdmin"
+    session_name = "Terraform"
+  }
+}
