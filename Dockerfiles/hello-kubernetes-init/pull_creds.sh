0d4c9014f51c5bb09f421bef2c244a3cd73fad19
Constantin Bugneac <cosbug@gmail.com>
2019-04-07 20:29:51 +0100

    update

--- Dockerfiles/hello-kubernetes-init/pull_creds.sh
+++ Dockerfiles/hello-kubernetes-init/pull_creds.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-set -x
+set -o errexit
+set -o nounset
 
 VAULT_ROLE="$1"
 VAULT_SECRET_PATH="$2"
@@ -9,5 +10,12 @@ CREDS_FILE="$3"
 CA_CERT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 JWT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/token"
 
-VAULT_TOKEN="$(vault write -field=token -ca-cert=@${CA_CERT_FILE} auth/kubernetes/login role=${VAULT_ROLE} jwt=@${JWT_FILE})"
-vault kv get -format json ${VAULT_SECRET_PATH} > "${CREDS_FILE}"
+export VAULT_ADDR="http://vault.internal:8200"
+
+for retry in {1..5}; do
+  export VAULT_TOKEN="$(vault write -field=token -ca-cert=@${CA_CERT_FILE} auth/kubernetes/login role=${VAULT_ROLE} jwt=@${JWT_FILE})" && break || sleep 3
+done
+
+for retry in {1..5}; do
+  vault kv get -format json ${VAULT_SECRET_PATH} > "${CREDS_FILE}" && break || sleep 3
+done
