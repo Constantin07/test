1502e2be62af8f069beba31da36bd3b630013beb
Constantin Bugneac <cosbug@gmail.com>
2019-04-07 20:42:50 +0100

    added argument check

--- Dockerfiles/hello-kubernetes-init/pull_creds.sh
+++ Dockerfiles/hello-kubernetes-init/pull_creds.sh
@@ -3,14 +3,16 @@
 set -o errexit
 set -o nounset
 
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters, expecting 3 but got $#."
+fi
+
 VAULT_ROLE="$1"
 VAULT_SECRET_PATH="$2"
 CREDS_FILE="$3"
 
 CA_CERT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 JWT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/token"
-
-export VAULT_ADDR="http://vault.internal:8200"
 
 for retry in {1..5}; do
   export VAULT_TOKEN="$(vault write -field=token -ca-cert=@${CA_CERT_FILE} auth/kubernetes/login role=${VAULT_ROLE} jwt=@${JWT_FILE})" && break || sleep 3
