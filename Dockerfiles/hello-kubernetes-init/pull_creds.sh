5bf83290556b198623aa7dbd1070943084dc8087
Constantin Bugneac <cosbug@gmail.com>
2019-04-07 20:45:24 +0100

    added argument check

--- Dockerfiles/hello-kubernetes-init/pull_creds.sh
+++ Dockerfiles/hello-kubernetes-init/pull_creds.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-set -o errexit
+#set -o errexit
 set -o nounset
-
-if [ "$#" -ne 3 ]; then
-    echo "Illegal number of parameters, expecting 3 but got $#."
-fi
 
 VAULT_ROLE="$1"
 VAULT_SECRET_PATH="$2"
@@ -15,9 +11,25 @@ CA_CERT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 JWT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/token"
 
 for retry in {1..5}; do
-  export VAULT_TOKEN="$(vault write -field=token -ca-cert=@${CA_CERT_FILE} auth/kubernetes/login role=${VAULT_ROLE} jwt=@${JWT_FILE})" && break || sleep 3
+    VAULT_TOKEN="$(vault write -field=token -ca-cert=@${CA_CERT_FILE} auth/kubernetes/login role=${VAULT_ROLE} jwt=@${JWT_FILE})"
+    if [ $? -ne 0 ]; then
+        echo "Failed to retrieve Vault token, attempt $retry, retrying in 3 seconds ..."
+        sleep 3
+    else
+        export VAULT_TOKEN
+        echo "Successfully retrived Vault token."
+        break
+    fi
 done
 
 for retry in {1..5}; do
-  vault kv get -format json ${VAULT_SECRET_PATH} > "${CREDS_FILE}" && break || sleep 3
+    vault kv get -format json ${VAULT_SECRET_PATH} > "${CREDS_FILE}"
+    if [ $? -ne 0 ]; then
+        echo "Failed to read secret from Vault, attempt $retry, retrying in 3 seconds ..."
+        sleep 3
+    else
+        echo "Successfully written secrets to ${CREDS_FILE}"
+        break
+    fi
 done
+
