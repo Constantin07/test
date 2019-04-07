93f99bc4a22c26cb21f644fd3fa7d565bfdb3f18
Constantin Bugneac <cosbug@gmail.com>
2019-04-07 19:35:57 +0100

    Added init container

---
+++ Dockerfiles/hello-kubernetes-init/pull_creds.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -x
+
+VAULT_ROLE="$1"
+VAULT_SECRET_PATH="$2"
+CREDS_FILE="$3"
+
+CA_CERT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+JWT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/token"
+
+VAULT_TOKEN="$(vault write -field=token -ca-cert=@${CA_CERT_FILE} auth/kubernetes/login role=${VAULT_ROLE} jwt=@${JWT_FILE})"
+SECRET="$(vault kv get -format json ${VAULT_SECRET_PATH})"
+
+echo "${SECRET}" > "$3"
