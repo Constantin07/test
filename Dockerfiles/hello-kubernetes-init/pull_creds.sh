0ebfbcac93df5092b6f1046441a99eb8ec4ee38b
Constantin Bugneac <cosbug@gmail.com>
2019-04-07 21:46:07 +0100

    minor update

--- Dockerfiles/hello-kubernetes-init/pull_creds.sh
+++ Dockerfiles/hello-kubernetes-init/pull_creds.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#set -o errexit
 set -o nounset
 
 VAULT_ROLE="$1"
@@ -10,22 +9,31 @@ CREDS_FILE="$3"
 CA_CERT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 JWT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/token"
 
-for retry in {1..5}; do
+#export VAULT_ADDR="http://vault.internal:8200"
+
+retries=5
+
+i=1
+while [ $i -le $retries ]; do
     VAULT_TOKEN="$(vault write -field=token -ca-cert=@${CA_CERT_FILE} auth/kubernetes/login role=${VAULT_ROLE} jwt=@${JWT_FILE})"
     if [ $? -ne 0 ]; then
-        echo "Failed to retrieve Vault token, attempt $retry, retrying in 3 seconds ..."
+        echo "Failure to retrieve Vault token, attempt $i, retrying in 3 seconds ..."
         sleep 3
     else
         export VAULT_TOKEN
-        echo "Successfully retrived Vault token."
+        echo "Successfully retrieved Vault token."
         break
     fi
+    i=$((i + 1))
 done
 
-for retry in {1..5}; do
+[[ $retries -eq $i ]] && { echo "Max retries reached, exiting ..."; exit 1; }
+
+i=1
+while [ $i -le $retries ]; do
     vault kv get -format json ${VAULT_SECRET_PATH} > "${CREDS_FILE}"
     if [ $? -ne 0 ]; then
-        echo "Failed to read secret from Vault, attempt $retry, retrying in 3 seconds ..."
+        echo "Failure to read secret from Vault, attempt $i, retrying in 3 seconds ..."
         sleep 3
     else
         echo "Successfully written secrets to ${CREDS_FILE}"
@@ -33,3 +41,4 @@ for retry in {1..5}; do
     fi
 done
 
+[[ $retries -eq $i ]] && { echo "Max retries reached, exiting ..."; exit 1; }
