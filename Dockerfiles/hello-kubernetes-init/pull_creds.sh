559d682adc559bdf5b1cadd168ba13f814d27eb8
Constantin Bugneac <cosbug@gmail.com>
2019-04-07 22:19:35 +0100

    image update to Ubuntu

--- Dockerfiles/hello-kubernetes-init/pull_creds.sh
+++ Dockerfiles/hello-kubernetes-init/pull_creds.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o nounset
 
@@ -9,16 +9,15 @@ CREDS_FILE="$3"
 CA_CERT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 JWT_FILE="/var/run/secrets/kubernetes.io/serviceaccount/token"
 
-#export VAULT_ADDR="http://vault.internal:8200"
-
-retries=5
+timeout=3
+retries=7
 
 i=1
 while [ $i -le $retries ]; do
     VAULT_TOKEN="$(vault write -field=token -ca-cert=@${CA_CERT_FILE} auth/kubernetes/login role=${VAULT_ROLE} jwt=@${JWT_FILE})"
     if [ $? -ne 0 ]; then
-        echo "Failure to retrieve Vault token, attempt $i, retrying in 3 seconds ..."
-        sleep 3
+        echo "Failure to retrieve Vault token, attempt $i, retrying in $timeout seconds ..."
+        sleep $timeout
     else
         export VAULT_TOKEN
         echo "Successfully retrieved Vault token."
@@ -33,8 +32,8 @@ i=1
 while [ $i -le $retries ]; do
     vault kv get -format json ${VAULT_SECRET_PATH} > "${CREDS_FILE}"
     if [ $? -ne 0 ]; then
-        echo "Failure to read secret from Vault, attempt $i, retrying in 3 seconds ..."
-        sleep 3
+        echo "Failure to read secret from Vault, attempt $i, retrying in $timeout seconds ..."
+        sleep $timeout
     else
         echo "Successfully written secrets to ${CREDS_FILE}"
         break
