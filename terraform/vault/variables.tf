62983257b2d8a0ca9b77f5a5b3ae2dc9a35a7416
Constantin Bugneac <cosbug@gmail.com>
2019-04-08 21:12:58 +0100

    minor updates

--- terraform/vault/variables.tf
+++ terraform/vault/variables.tf
@@ -2,3 +2,7 @@ variable "kube_config_file" {
   description = "Path to kubectl configuration file."
   default     = "~/.kube/config"
 }
+
+variable "token_reviewer_jwt" {
+  description = "Token associated with vault-auth service account in K8s."
+}
