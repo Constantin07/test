2afba51a81b7c7846e5e3c4e417a0330de6d8b01
Constantin Bugneac <cosbug@gmail.com>
2019-06-06 23:33:14 +0100

    Make app talk to Vault
    Signed-off-by: Constantin Bugneac <cosbug@gmail.com>

--- Dockerfiles/hello-kubernetes/server.js
+++ Dockerfiles/hello-kubernetes/server.js
@@ -12,10 +12,28 @@ app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 app.use(morgan('combined'));
 
+// Vault
+const kubernetesCaCert = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt', 'utf8');
+const servicAccountSecretToken = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
+
+var vault_role = process.env.VAULT_ROLE;
+var vault_secret_path = process.env.VAULT_SECRET_PATH;
+var vault = require("node-vault")();
+var creds;
+
+process.env.VAULT_SKIP_VERIFY = 'true';
+vault.write('auth/kubernetes/login', { ca_cert: kubernetesCaCert, jwt: servicAccountSecretToken, role: vault_role})
+  .then((result) => {
+  vault.token = result.auth.client_token;
+  vault.read(vault_secret_path)
+  .then((res) => creds = res.data)
+  .catch((err) => console.error(err.message));
+});
+
 // Configuration
 var port = process.env.PORT || 8080;
 var message = process.env.MESSAGE || "Hello world!";
-var creds = JSON.parse(fs.readFileSync('/secrets/creds.json'));
+//var creds = JSON.parse(fs.readFileSync('/secrets/creds.json'));
 
 app.get('/', function (req, res) {
     res.render('home', {
@@ -23,7 +41,8 @@ app.get('/', function (req, res) {
 	platform: os.type(),
 	release: os.release(),
 	hostName: os.hostname(),
-	creds: JSON.stringify(creds['data'])
+	creds: JSON.stringify(creds)
+	//creds: JSON.stringify(creds['data'])
     });
 });
 
