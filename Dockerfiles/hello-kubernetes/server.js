3086b269f8564619b0769fc314d34fe2546450a6
Constantin Bugneac <cosbug@gmail.com>
2019-06-25 00:18:54 +0100

    Updated hello-kubernetes

--- Dockerfiles/hello-kubernetes/server.js
+++ Dockerfiles/hello-kubernetes/server.js
@@ -5,30 +5,34 @@ var os = require("os");
 var morgan  = require('morgan');
 var router = express.Router();
 
-const fs = require('fs');
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 app.use(morgan('combined'));
 
 // Vault
+const fs = require('fs');
 const kubernetesCaCert = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt', 'utf8');
-const servicAccountSecretToken = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
+const serviceAccountSecretToken = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
 
 var vault_role = process.env.VAULT_ROLE;
 var vault_secret_path = process.env.VAULT_SECRET_PATH;
 var vault = require("node-vault")();
 var creds;
 
+//process.env.DEBUG = 'node-vault'; // switch on debug mode
+
 process.env.VAULT_SKIP_VERIFY = 'true';
-vault.write('auth/kubernetes/login', { ca_cert: kubernetesCaCert, jwt: servicAccountSecretToken, role: vault_role})
-  .then((result) => {
+vault.kubernetesLogin({role: vault_role, jwt: serviceAccountSecretToken, ca_cert: kubernetesCaCert})
+.then((result) => {
+  //console.log(result);
   vault.token = result.auth.client_token;
   vault.read(vault_secret_path)
-  .then((res) => creds = res.data)
-  .catch((err) => console.error(err.message));
-});
+    .then((res) => {
+      creds = res.data
+    })
+})
+.catch((err) => console.error(err.message));
 
 // Configuration
 var port = process.env.PORT || 8080;
@@ -41,6 +45,7 @@ app.get('/', function (req, res) {
 	platform: os.type(),
 	release: os.release(),
 	hostName: os.hostname(),
+	vaultRole: vault_role,
 	creds: JSON.stringify(creds)
 	//creds: JSON.stringify(creds['data'])
     });
