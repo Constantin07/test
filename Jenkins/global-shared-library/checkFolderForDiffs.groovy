83f45674d6fefab54095ec2a9751616a514abd6d
Constantin Bugneac <cosbug@gmail.com>
2019-08-03 10:41:04 +0100

    Added groovy function

---
+++ Jenkins/global-shared-library/checkFolderForDiffs.groovy
@@ -0,0 +1,14 @@
+#!groovy
+
+/* Checks a folder if changed in the latest commit.
+ * Returns true if changed, or false if no changes.
+*/
+def call(String path) {
+  try {
+    // git diff will return 1 for changes (failure) which is caught in catch, or 0 meaning no changes
+    sh "git diff --quiet --exit-code HEAD~1..HEAD ${path}"
+    return false
+  } catch (err) {
+    return true
+  }
+}
