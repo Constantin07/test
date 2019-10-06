5a306bd82f85ccfb0350e4fa7bb5622a96747b03
Constantin Bugneac <cosbug@gmail.com>
2019-10-06 16:21:18 +0100

    Amend tests

--- Jenkins/kubernetes/bootstrap/7_cluster_tests/cluster_status_tests.sh
+++ Jenkins/kubernetes/bootstrap/7_cluster_tests/cluster_status_tests.sh
@@ -2,23 +2,30 @@
 
 oneTimeSetUp() {
   assertNotNull "Required environment variable KUBECONFIG is not set" "${KUBECONFIG}"
-  status=`kubectl get componentstatuses`
+  APISERVER=`kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " "`
+  # Disable as being depecated
+  #status=`kubectl get componentstatuses`
 }
 
-test_ControllerManagerStatus() {
-  cm_status=`echo "${status}" | grep controller-manager | awk '{print $2}'`
-  assertEquals "Controller Manager is not in Healthy state" 'Healthy' "${cm_status}"
+test_ApiServerHealthCheck() {
+  cluster_conn=`curl -ksSL ${APISERVER}/healthz`
+  assertEquals "Cluster API health returned non-ok state" 'ok' "${cluster_conn}"
 }
 
-test_SchedulerStatus() {
-  s_status=`echo "${status}" | grep scheduler | awk '{print $2}'`
-  assertEquals "Scheduler is not in Healthy state" 'Healthy' "${s_status}"
-}
+#test_ControllerManagerStatus() {
+#  cm_status=`echo "${status}" | grep controller-manager | awk '{print $2}'`
+#  assertEquals "Controller Manager is not in Healthy state" 'Healthy' "${cm_status}"
+#}
 
-test_EtcdStatus() {
-  e_status=`echo "${status}" | grep etcd | awk '{print $2}'`
-  assertEquals "Etcd is not in Healthy state" 'Healthy' "${e_status}"
-}
+#test_SchedulerStatus() {
+#  s_status=`echo "${status}" | grep scheduler | awk '{print $2}'`
+#  assertEquals "Scheduler is not in Healthy state" 'Healthy' "${s_status}"
+#}
+
+#test_EtcdStatus() {
+#  e_status=`echo "${status}" | grep etcd | awk '{print $2}'`
+#  assertEquals "Etcd is not in Healthy state" 'Healthy' "${e_status}"
+#}
 
 # Load shUnit2
 . /usr/local/bin/shunit2
