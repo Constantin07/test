d1f3a20d8b1d20c39f10bc6677d9d02d624725c9
Constantin Bugneac <cosbug@gmail.com>
2019-10-06 17:17:50 +0100

    updated hello-kubernetes app

--- Jenkins/dockerfiles/hello-kubernetes/server.js
+++ Jenkins/dockerfiles/hello-kubernetes/server.js
@@ -10,36 +10,26 @@ app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 app.use(morgan('combined'));
 
-
-// Vault
-const fs = require('fs');
-const kubernetesCaCert = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt', 'utf8');
-const serviceAccountSecretToken = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
-
-var vault_role = process.env.VAULT_ROLE;
 var vault_secret_path = process.env.VAULT_SECRET_PATH;
-var vault = require("node-vault")();
+var options = { token: ''};
+var vault = require("node-vault")(options);
 var creds;
 
 // switch on debug mode
 //process.env.DEBUG = 'node-vault';
-process.env.VAULT_SKIP_VERIFY = 'true';
 
-vault.kubernetesLogin({role: vault_role, jwt: serviceAccountSecretToken, ca_cert: kubernetesCaCert})
-.then((result) => {
-  //console.log(result);
-  vault.token = result.auth.client_token;
+console.log('Reading Vault secrets');
+setTimeout(function() {
   vault.read(vault_secret_path)
-    .then((res) => {
-      creds = res.data
-    })
-})
-.catch((err) => console.error(err.message));
+  .then((result) => {
+    creds = result.data
+  })
+  .catch((err) => console.error(err.message));
+}, 5000);
 
 // Configuration
 var port = process.env.PORT || 8080;
 var message = process.env.MESSAGE || "Hello world!";
-//var creds = JSON.parse(fs.readFileSync('/secrets/creds.json'));
 
 app.get('/', function (req, res) {
     // Get client IP
@@ -50,10 +40,8 @@ app.get('/', function (req, res) {
 	platform: os.type(),
 	release: os.release(),
 	hostName: os.hostname(),
-	vaultRole: vault_role,
 	xForwardedFor: x_forwarded_for,
 	creds: JSON.stringify(creds)
-	//creds: JSON.stringify(creds['data'])
     });
 });
 
