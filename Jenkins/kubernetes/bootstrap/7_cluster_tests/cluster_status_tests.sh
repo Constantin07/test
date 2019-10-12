449bc112b8a553479c7afd31a76156df41c88df2
Constantin Bugneac <cosbug@gmail.com>
2019-10-12 18:00:45 +0100

    Updated tests

--- Jenkins/kubernetes/bootstrap/7_cluster_tests/cluster_status_tests.sh
+++ Jenkins/kubernetes/bootstrap/7_cluster_tests/cluster_status_tests.sh
@@ -3,8 +3,7 @@
 oneTimeSetUp() {
   assertNotNull "Required environment variable KUBECONFIG is not set" "${KUBECONFIG}"
   APISERVER=`kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " "`
-  # Disable as being depecated
-  #status=`kubectl get componentstatuses`
+  nodes=`kubectl get nodes -o=jsonpath='{.items[*].metadata.name}'`
 }
 
 test_ApiServerEndpointHealthCheck() {
@@ -12,20 +11,40 @@ test_ApiServerEndpointHealthCheck() {
   assertEquals "Cluster API health returned non-ok state" 'ok' "${cluster_conn}"
 }
 
-#test_ControllerManagerStatus() {
-#  cm_status=`echo "${status}" | grep controller-manager | awk '{print $2}'`
-#  assertEquals "Controller Manager is not in Healthy state" 'Healthy' "${cm_status}"
-#}
+test_NodeReadinessCheck() {
+  for node in $nodes; do
+    node_status=`kubectl get node ${node} -o=jsonpath='{.status.conditions[?(@.type=="Ready")].status}'`
+    assertEquals "Node ${node} is not in Ready state" 'True' "${node_status}"
+  done
+}
 
-#test_SchedulerStatus() {
-#  s_status=`echo "${status}" | grep scheduler | awk '{print $2}'`
-#  assertEquals "Scheduler is not in Healthy state" 'Healthy' "${s_status}"
-#}
+test_NodeNetworkUnavailableCheck() {
+  for node in $nodes; do
+    node_status=`kubectl get node ${node} -o=jsonpath='{.status.conditions[?(@.type=="NetworkUnavailable")].status}'`
+    assertEquals "Node ${node} is in NetworkUnavailable state" 'False' "${node_status}"
+  done
+}
 
-#test_EtcdStatus() {
-#  e_status=`echo "${status}" | grep etcd | awk '{print $2}'`
-#  assertEquals "Etcd is not in Healthy state" 'Healthy' "${e_status}"
-#}
+test_NodeMemoryPressureCheck() {
+  for node in $nodes; do
+    node_status=`kubectl get node ${node} -o=jsonpath='{.status.conditions[?(@.type=="MemoryPressure")].status}'`
+    assertEquals "Node ${node} is in MemoryPressure state" 'False' "${node_status}"
+  done
+}
+
+test_NodeDiskPressureCheck() {
+  for node in $nodes; do
+    node_status=`kubectl get node ${node} -o=jsonpath='{.status.conditions[?(@.type=="DiskPressure")].status}'`
+    assertEquals "Node ${node} is in DiskPressure state" 'False' "${node_status}"
+  done
+}
+
+test_NodePIDPressureCheck() {
+  for node in $nodes; do
+    node_status=`kubectl get node ${node} -o=jsonpath='{.status.conditions[?(@.type=="PIDPressure")].status}'`
+    assertEquals "Node ${node} is in PIDPressure state" 'False' "${node_status}"
+  done
+}
 
 # Load shUnit2
 . /usr/local/bin/shunit2
