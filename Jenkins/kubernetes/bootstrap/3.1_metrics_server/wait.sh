afcccc5af9d8d6286150c9731e29a3d350a484d7
Constantin Bugneac <cosbug@gmail.com>
2019-10-13 19:12:10 +0100

    added script to wait for metrics server.

---
+++ Jenkins/kubernetes/bootstrap/3.1_metrics_server/wait.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -u
+
+timeout=5
+max_retry=24
+count=1
+RC=1
+until [[ ${RC} -eq 0 || ${count} -eq ${max_retry} ]]; do
+    result=`kubectl top node 2>&1`
+    RC=$?
+    echo "${result}"
+    echo "Waiting for metrics-server to become available, next retry ${count} in ${timeout} seconds."
+    sleep ${timeout}
+    ((count++))
+done
+
+if [ ${count} -eq ${max_retry} ]; then
+    echo "Maximum ${max_retry} retrie(s) reached. Exiting ..."
+    exit 1
+fi
