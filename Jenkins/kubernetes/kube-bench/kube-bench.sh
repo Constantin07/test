2e580d38bd819e2986cf733dfec6073c465f2bc8
Constantin Bugneac <cosbug@gmail.com>
2019-11-18 22:56:27 +0000

    Updated kube-bench

--- Jenkins/kubernetes/kube-bench/kube-bench.sh
+++ Jenkins/kubernetes/kube-bench/kube-bench.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <master|node>"
     exit 1
@@ -8,16 +10,22 @@ fi
 TARGET=$1
 NAMESPACE="${NAMESPACE:-default}"
 JOB_NAME="kube-bench-${TARGET}"
-TIMEOUT="60s"
+TIMEOUT="90s"
 
-# Run kube-bench on node/master
-kubectl apply -n ${NAMESPACE} -f job-${TARGET}.yaml
+# Validate definition files
+kubeval --strict ./kube-bench-${TARGET}-configmap.yaml
+kubeval --strict ./job-${TARGET}.yaml
+
+# Run kube-bench job on node/master
+kubectl apply -n "${NAMESPACE}" -f ./kube-bench-${TARGET}-configmap.yaml
+kubectl apply -n "${NAMESPACE}" -f ./job-${TARGET}.yaml
 
 # Wait for job completion
-kubectl -n ${NAMESPACE} wait --for=condition=complete --timeout=${TIMEOUT} job/${JOB_NAME}
+kubectl -n "${NAMESPACE}" wait --for=condition=complete --timeout=${TIMEOUT} job/${JOB_NAME}
 
 # Get the report
-kubectl -n ${NAMESPACE} logs job/${JOB_NAME}
+kubectl -n "${NAMESPACE}" logs job/${JOB_NAME}
 
 # Delete job
-kubectl -n ${NAMESPACE} delete job/${JOB_NAME}
+kubectl -n "${NAMESPACE}" delete -f ./job-${TARGET}.yaml
+kubectl -n "${NAMESPACE}" delete -f ./kube-bench-${TARGET}-configmap.yaml
