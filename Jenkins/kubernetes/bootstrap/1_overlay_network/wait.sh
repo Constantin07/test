ccf60b9e118db21514fc9c6252f561d897df574d
Constantin Bugneac <cosbug@gmail.com>
2020-06-05 09:12:10 +0100

    Updated alpine & helm

--- Jenkins/kubernetes/bootstrap/1_overlay_network/wait.sh
+++ Jenkins/kubernetes/bootstrap/1_overlay_network/wait.sh
@@ -3,7 +3,7 @@
 set -eu
 
 NAMESPACE="${NAMESPACE:-kube-system}"
-TIMEOUT="180s"
+TIMEOUT="240s"
 
 echo "Wait for calico-node to be deployed"
 kubectl -n "${NAMESPACE}" rollout status "ds/calico-node" --watch=true --timeout=${TIMEOUT}
