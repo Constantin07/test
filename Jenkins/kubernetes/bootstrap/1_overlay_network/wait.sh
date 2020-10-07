d55c84b252b65cb387c2fbfad41207595a4460e7
Constantin Bugneac <cosbug@gmail.com>
2020-10-07 09:11:16 +0100

    Updated Calico version

--- Jenkins/kubernetes/bootstrap/1_overlay_network/wait.sh
+++ Jenkins/kubernetes/bootstrap/1_overlay_network/wait.sh
@@ -3,7 +3,7 @@
 set -eu
 
 NAMESPACE="${NAMESPACE:-kube-system}"
-TIMEOUT="300s"
+TIMEOUT="360s"
 
 echo "Wait for calico-node to be deployed"
 kubectl -n "${NAMESPACE}" rollout status "ds/calico-node" --watch=true --timeout=${TIMEOUT}
