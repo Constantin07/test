af49ed4d34e6b923a594605a3010503ed6e2a0df
Constantin Bugneac <cosbug@gmail.com>
2020-10-15 20:54:54 +0100

    added docker creds to K8s

---
+++ pipelines/kubernetes/bootstrap/2.1_docker_credentials/regcred.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Create docker-registry crednetials to allow K8s to pull images from private registries.
+
+set -eu -o pipefail
+
+: ${DOCKERHUB_USERNAME:?variable is not defined}
+: ${DOCKERHUB_PASSWORD:?variable is not defined}
+
+DOCKERHUB_SERVER='https://index.docker.io/v1/'
+
+# List of namespaces to add docker credentials to
+NAMESPACES=(
+  default
+)
+
+for ns in "${NAMESPACES[@]}"; do
+  echo -n "Namespace: $ns, "
+  kubectl -n "$ns" create secret docker-registry dockerhub \
+    --docker-server="${DOCKERHUB_SERVER}" \
+    --docker-username="${DOCKERHUB_USERNAME}" \
+    --docker-password="${DOCKERHUB_PASSWORD}" --dry-run=client -o yaml | kubectl apply -f -
+done
