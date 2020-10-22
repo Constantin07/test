b3030ac46c32be7c43458894b482927e3339af33
Constantin Bugneac <cosbug@gmail.com>
2020-10-22 18:34:01 +0100

    increase wait time

--- pipelines/kubernetes/bootstrap/1_overlay_network/wait.sh
+++ pipelines/kubernetes/bootstrap/1_overlay_network/wait.sh
@@ -3,10 +3,11 @@
 set -eu
 
 NAMESPACE="${NAMESPACE:-kube-system}"
-TIMEOUT="360s"
+TIMEOUT="600s"
+
+echo "Wait for calico-kube-controllers to be deployed"
+kubectl -n "${NAMESPACE}" rollout status "deployment/calico-kube-controllers" --watch=true --timeout=${TIMEOUT}
 
 echo "Wait for calico-node to be deployed"
 kubectl -n "${NAMESPACE}" rollout status "ds/calico-node" --watch=true --timeout=${TIMEOUT}
 
-echo "Wait for calico-kube-controllers to be deployed"
-kubectl -n "${NAMESPACE}" rollout status "deployment/calico-kube-controllers" --watch=true --timeout=${TIMEOUT}
