9dd414d090c8fa851d9701d35837eb7dc8b2d81c
Constantin Bugneac <cosbug@gmail.com>
2020-10-22 18:34:01 +0100

    increase wait time

---
+++ pipelines/kubernetes/hello-kubernetes/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SITE='http://hello-kubernetes.internal:30000'
+
+test_HelloKubernetesIsUp() {
+  result=`curl -sSf --retry 3 -o /dev/null -w "%{http_code}" "$SITE"`
+  assertEquals "Site is up" "${result}" '200'
+}
+
+# Load shUnit2
+. /usr/local/bin/shunit2
