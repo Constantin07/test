da682f9730020700b8f5cf72e3c4abc132f7bf72
Constantin Bugneac <cosbug@gmail.com>
2020-11-16 23:27:13 +0000

    added rate-limit quotas

--- terraform/vault/config/main.tf
+++ terraform/vault/config/main.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  version = "~> 2.12.2"
+  version = "2.15.0"
 }
 
 resource "vault_audit" "file" {
@@ -23,7 +23,7 @@ resource "vault_auth_backend" "aws" {
   type        = "aws"
   description = "AWS authentication"
   tune {
-    default_lease_ttl  = "3600s"
+    default_lease_ttl  = "360s"
     max_lease_ttl      = "3600s"
     listing_visibility = "unauth"
   }
@@ -38,3 +38,12 @@ resource "vault_auth_backend" "kubernetes" {
     listing_visibility = "unauth"
   }
 }
+
+
+## Policies
+
+resource "vault_policy" "hello_kubernetes" {
+  name   = "hello-kubernetes"
+  policy = file("${path.module}/policies/policy_hello_kubernetes.hcl")
+}
+
--- terraform/vault/config/policies.tf
+++
@@ -1,6 +0,0 @@
-# Vault policies
-
-resource "vault_policy" "hello_kubernetes" {
-  name   = "hello-kubernetes"
-  policy = file("${path.module}/policies/policy_hello_kubernetes.hcl")
-}
