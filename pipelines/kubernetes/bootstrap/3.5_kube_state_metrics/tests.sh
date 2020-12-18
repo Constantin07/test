1ad83b40339fcbd6178847a44cdbd6af08f610d2
Constantin Bugneac <cosbug@gmail.com>
2020-12-18 18:59:19 +0000

    added test

---
+++ pipelines/kubernetes/bootstrap/3.5_kube_state_metrics/tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+trap "kill 0" ERR INT TERM
+
+oneTimeSetUp() {
+  assertNotNull "Required environment variable KUBECONFIG is not set" "${KUBECONFIG}"
+  assertNotNull "Required environment variable NAMESPACE is not set" "${NAMESPACE}"
+  echo "Wait for kube-state-metrics to be up and running"
+  kubectl -n "${NAMESPACE}" rollout status deploy/kube-state-metrics --timeout=120s --watch=true
+  echo "Connect to kube-state-metrics"
+  kubectl port-forward -n "${NAMESPACE}" svc/kube-state-metrics 9100:8080 &>/dev/null &
+  export PID=$!
+}
+
+oneTimeTearDown() {
+  ps -p $PID > /dev/null && kill $PID
+  return 0
+}
+
+test_KubeStateMetricsIsWorking() {
+  sleep 2
+  result=`curl -sSf http://127.0.0.1:9100`
+  assertContains "kube-state-metrics contains metrics" "${result}" 'metrics'
+}
+
+# Load shUnit2
+. /usr/local/bin/shunit2
