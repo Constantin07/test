5a39337e9357767b944ef9e134f64d494ed81d82
Constantin Bugneac <cosbug@gmail.com>
2021-05-20 19:43:31 +0100

    rename dockerhub creds

--- pipelines/kubernetes/bootstrap/2.1_docker_credentials/regcred.sh
+++ pipelines/kubernetes/bootstrap/2.1_docker_credentials/regcred.sh
@@ -5,7 +5,7 @@
 set -eu -o pipefail
 
 : ${DOCKERHUB_USERNAME:?variable is not defined}
-: ${DOCKERHUB_PASSWORD:?variable is not defined}
+: ${DOCKERHUB_TOKEN:?variable is not defined}
 
 DOCKERHUB_SERVER='https://index.docker.io/v1/'
 
@@ -19,5 +19,5 @@ for ns in "${NAMESPACES[@]}"; do
   kubectl -n "$ns" create secret docker-registry dockerhub \
     --docker-server="${DOCKERHUB_SERVER}" \
     --docker-username="${DOCKERHUB_USERNAME}" \
-    --docker-password="${DOCKERHUB_PASSWORD}" --dry-run=client -o yaml | kubectl apply -f -
+    --docker-password="${DOCKERHUB_TOKEN}" --dry-run=client -o yaml | kubectl apply -f -
 done
