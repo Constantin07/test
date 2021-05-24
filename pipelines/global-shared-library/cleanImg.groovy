7fc053a169d3bd0e8b4e6b1e44dcd582c8d78c27
Constantin Bugneac <cosbug@gmail.com>
2021-05-24 23:29:30 +0100

    updated cleanup script

--- pipelines/global-shared-library/cleanImg.groovy
+++ pipelines/global-shared-library/cleanImg.groovy
@@ -4,6 +4,6 @@
 
 def call() {
   withEnv(['PATH+img=/usr/local/bin']) {
-    sh "img ls | tail -n +2 | cut -f1 | xargs -r img rm"
+    sh "img ls | tail -n +2 | cut -f1 | xargs -r -I % sh -c '{ img rm %; sleep 1; }'"
   }
 }
