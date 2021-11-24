dc597358aee41503e8befe40b442bf2235a368c2
Constantin Bugneac <cosbug@gmail.com>
2021-11-24 21:00:07 +0000

    added script to delete docker images

---
+++ scripts/del_image.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Used to list and delete docker images from v2 registry
+
+registry='centos7.internal:5000'
+name=$1
+tag=$2
+curl -sSL "http://${registry}/v2/${name}/tags/list" | jq
+
+curl -v -sSL -X DELETE "http://${registry}/v2/${name}/manifests/$(
+    curl -sSL -I \
+        -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+        "http://${registry}/v2/${name}/manifests/${tag}" \
+    | awk '$1 == "Docker-Content-Digest:" { print $2 }' \
+    | tr -d $'\r' \
+)"
