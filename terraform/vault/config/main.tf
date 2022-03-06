fff1198d4dc46fb59e7a9f7c79aef9da1436fb17
Constantin Bugneac <cosbug@gmail.com>
2022-03-06 21:03:38 +0000

    add approle auth to vault

--- terraform/vault/config/main.tf
+++ terraform/vault/config/main.tf
@@ -1,25 +1,3 @@
 # Requires VAULT_ADDR and VAULT_TOKEN environment variables to be set.
 provider "vault" {
 }
-
-## Auth
-
-resource "vault_auth_backend" "aws" {
-  type        = "aws"
-  description = "AWS authentication backend"
-  tune {
-    default_lease_ttl  = "360s"
-    max_lease_ttl      = "3600s"
-    listing_visibility = "unauth"
-  }
-}
-
-resource "vault_auth_backend" "userpass" {
-  type        = "userpass"
-  description = "Userpass authentication backend"
-  tune {
-    default_lease_ttl  = "3600s"
-    max_lease_ttl      = "28800s"
-    listing_visibility = "unauth"
-  }
-}
