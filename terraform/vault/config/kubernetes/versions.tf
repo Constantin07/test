f504c35852f8de2d4a0cf8bc980b1865548da21c
Constantin Bugneac <cosbug@gmail.com>
2022-04-05 07:31:39 +0100

    added consult-template binary

--- terraform/vault/config/kubernetes/versions.tf
+++ terraform/vault/config/kubernetes/versions.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     external = {
       source  = "hashicorp/external"
-      version = "2.1.0"
+      version = "2.2.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.6.1"
+      version = "2.9.0"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "3.3.1"
+      version = "3.4.1"
     }
   }
-  required_version = "~> 1.0"
+  required_version = "~> 1.1"
 }
--- terraform/vault/config/versions.tf
+++ terraform/vault/config/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vault = {
       source  = "hashicorp/vault"
-      version = "3.3.1"
+      version = "3.4.1"
     }
   }
   required_version = "~> 1.1"
