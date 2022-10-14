709e3ff254e21d09421da9a45bb27e1d1c430578
Constantin Bugneac <cosbug@gmail.com>
2022-10-14 15:57:02 +0100

    bump calico

--- terraform/vault/config/kubernetes/versions.tf
+++ terraform/vault/config/kubernetes/versions.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     external = {
       source  = "hashicorp/external"
-      version = "2.2.2"
+      version = "2.2.3"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.13"
+      version = "~> 2.15"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 3.8"
+      version = "~> 3.10"
     }
   }
   required_version = "~> 1.3"
