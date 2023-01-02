579eeea38942f0c8ad899e75e13fdddeed3d0937
Constantin Bugneac <cosbug@gmail.com>
2023-01-02 20:18:47 +0000

    bump helfile version

--- pipelines/dockerfiles/tool-box/add_ca_certs.sh
+++ pipelines/dockerfiles/tool-box/add_ca_certs.sh
@@ -5,7 +5,7 @@
 set -e
 
 for cert_path in pki pki_int; do
-    # Skip pulling certs when run via GitHub Actions
+    # Skip pulling certs from Vault when running via GitHub Workflow
     if [ -z "$GITHUB_WORKFLOW" ]; then
 	curl -sSf -o "${cert_path}_ca.crt" "${VAULT_ADDR}/v1/${cert_path}/ca/pem"
     fi
