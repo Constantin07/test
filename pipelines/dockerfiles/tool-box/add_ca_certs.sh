9db9f4939fc9e939d57d17508c69000fd35f9dfd
Constantin Bugneac <cosbug@gmail.com>
2023-01-02 16:50:34 +0000

    run some pipelines on-demand

--- pipelines/dockerfiles/tool-box/add_ca_certs.sh
+++ pipelines/dockerfiles/tool-box/add_ca_certs.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # This script adds root and intermediate CA certs to system trust store
 
 set -e
 
 for cert_path in pki pki_int; do
-    curl -sSf -o "${cert_path}_ca.crt" "${VAULT_ADDR}/v1/${cert_path}/ca/pem"
+    # Skip pulling certs when run via GitHub Actions
+    if [ -z "$GITHUB_WORKFLOW" ]; then
+	curl -sSf -o "${cert_path}_ca.crt" "${VAULT_ADDR}/v1/${cert_path}/ca/pem"
+    fi
 done
