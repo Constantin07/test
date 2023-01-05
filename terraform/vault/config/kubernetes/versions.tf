3e89346a91ff565acc1058aa8a3a794f3697a170
Constantin Bugneac <cosbug@gmail.com>
2023-01-05 23:19:48 +0000

    update tf vault provider

--- terraform/vault/config/kubernetes/versions.tf
+++ terraform/vault/config/kubernetes/versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 3.11"
+      version = "~> 3.12"
     }
   }
   required_version = "~> 1.3"
--- terraform/vault/config/versions.tf
+++ terraform/vault/config/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 3.8"
+      version = "~> 3.12"
     }
   }
   required_version = "~> 1.3"
