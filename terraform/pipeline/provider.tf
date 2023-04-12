07fc8c4f84204ed29a1e68a28ebdd215ea497a56
Constantin Bugneac <cosbug@gmail.com>
2023-04-12 22:57:58 +0100

    provider update

--- terraform/pipeline/provider.tf
+++ terraform/pipeline/provider.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.49"
+      version = ">= 4.62"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
     }
   }
 }
