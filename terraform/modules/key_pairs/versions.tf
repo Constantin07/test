e26b758e47ec151c3a247cad1d2df8834a01cf8c
Constantin Bugneac <cosbug@gmail.com>
2023-04-14 19:16:28 +0100

    fix tflint issue

--- terraform/modules/key_pairs/versions.tf
+++ terraform/modules/key_pairs/versions.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_version = ">= 1.3"
+
   required_providers {
     tls = {
       source  = "hashicorp/tls"
       version = ">= 4.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.60"
     }
   }
 }
