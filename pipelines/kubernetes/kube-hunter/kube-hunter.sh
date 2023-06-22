674df617e4a3df8256cb55dd0e3b958d2a1f7f81
Constantin Bugneac <cosbug@gmail.com>
2023-06-22 17:29:36 +0100

    add kube-hunter

---
+++ pipelines/kubernetes/kube-hunter/kube-hunter.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+NAMESPACE="${NAMESPACE:-default}"
+JOB_NAME="kube-hunter"
+TIMEOUT="120s"
+
+function cleanup() {
+    kubectl -n "${NAMESPACE}" delete job/${JOB_NAME} --ignore-not-found=true
+}
+
+trap cleanup ERR
+
+echo "Run kube-hunter in ${NAMESPACE} namespace."
+kubectl create -n "${NAMESPACE}" -f job.yaml
+
+echo "Waiting for job completion ..."
+kubectl -n "${NAMESPACE}" wait --for=condition=complete --timeout=${TIMEOUT} job/${JOB_NAME}
+
+echo "Getting report"
+kubectl -n "${NAMESPACE}" logs job/${JOB_NAME}
+
+cleanup
