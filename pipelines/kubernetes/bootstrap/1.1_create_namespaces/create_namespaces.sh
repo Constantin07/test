86bfa91e8df2c71e510aef3adad5f5ab13bb7fdc
Constantin Bugneac <cosbug@gmail.com>
2023-06-22 20:27:09 +0100

    remove automount for default sa in namespace

--- pipelines/kubernetes/bootstrap/1.1_create_namespaces/create_namespaces.sh
+++ pipelines/kubernetes/bootstrap/1.1_create_namespaces/create_namespaces.sh
@@ -2,7 +2,9 @@
 
 set -e -o pipefail
 
+echo "Create namespaces"
 kubectl create ns system -o yaml --dry-run=client | kubectl apply -f -
 kubectl label ns system name=system --overwrite=true
 
+# Declarative way
 kubectl apply -f namespaces.yaml
