22e97719f3fc927a0ecf970a757a96113c2f468b
Constantin Bugneac <cosbug@gmail.com>
2023-06-26 22:24:47 +0100

    Aded infra test for exernal-dns

---
+++ pipelines/kubernetes/bootstrap/3.3_external_dns/test/external_dns_test.go
@@ -0,0 +1,63 @@
+package test
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/retry"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	name               = "hello-world"   // name of K8s test resources
+	namespace          = "default"       // namespace to run tests in
+	maxRetries         = 20              // number of retries
+	timeBetweenRetries = 3 * time.Second // seconds between retries
+	host               = "hello-world.internal"
+)
+
+func TestExternalDNS(t *testing.T) {
+	t.Parallel()
+
+	// Path to the Kubernetes resource config we will test.
+	kubeResourcePath := "./test_deployment.yaml"
+
+	// Setup the kubectl config and context.
+	options := k8s.NewKubectlOptions("", "", namespace)
+
+	// At the end of the test, clean up any resources that were created.
+	defer k8s.KubectlDelete(t, options, kubeResourcePath)
+
+	// Deploy test resources. Fail the test if there are any errors.
+	k8s.KubectlApply(t, options, kubeResourcePath)
+
+	// Wait for all pods to be ready.
+	k8s.WaitUntilDeploymentAvailable(t, options, name, maxRetries, timeBetweenRetries)
+
+	// Wait for service endpoints is ready to accept traffic.
+	k8s.WaitUntilServiceAvailable(t, options, name, maxRetries, timeBetweenRetries)
+
+	// Wait for ingress resource to have an endpoint provisioned for it.
+	k8s.WaitUntilIngressAvailable(t, options, name, maxRetries, timeBetweenRetries)
+	//ingress := k8s.GetIngress(t, options, name)
+
+	url := fmt.Sprintf("https://%s", host)
+	fmt.Println(url)
+
+	retry.DoWithRetry(t, "Wait for FQDN to be resolvable", maxRetries, timeBetweenRetries, func() (string, error) {
+		_, err := net.LookupIP(host)
+		assert.Nil(t, err)
+		return "", err
+	})
+
+	// Make an HTTPS request to the URL and make sure it returns a 200 OK with the body "Hello, World".
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	http_helper.HttpGetWithRetry(t, url, tlsConfig, 200, "Hello world!", maxRetries, timeBetweenRetries)
+}
