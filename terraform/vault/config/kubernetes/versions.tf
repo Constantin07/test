f65cb447f0273daa5b63088c4d53dfa704307459
Constantin Bugneac <cosbug@gmail.com>
2023-11-01 23:33:16 +0000

    update vault

--- terraform/vault/config/kubernetes/versions.tf
+++ terraform/vault/config/kubernetes/versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 3.20"
+      version = "~> 3.21"
     }
   }
 }
--- terraform/vault/config/pki_scripts/add_ca_certs.sh
+++ terraform/vault/config/pki_scripts/add_ca_certs.sh
@@ -2,17 +2,35 @@
 
 # This script adds Root and Intermediate CA certs to system truster store
 
-set -e
+set -eo pipefail
 
 ROOT_CA_PATH="pki"
 INTERMEDIATE_CA_PATH="pki_int"
 
-# For CentOS Linux
-INSTALL_DIR="/usr/share/pki/ca-trust-source/anchors"
+echo "Getting CA certs from Vault"
+curl -sSf $VAULT_ADDR/v1/${ROOT_CA_PATH}/ca/pem -o root_ca.crt
+curl -sSf $VAULT_ADDR/v1/${INTERMEDIATE_CA_PATH}/ca/pem -o int_ca.crt
 
-curl -sSf $VAULT_ADDR/v1/${ROOT_CA_PATH}/ca/pem -o root_ca.pem
-curl -sSf $VAULT_ADDR/v1/${INTERMEDIATE_CA_PATH}/ca/pem -o int_ca.pem
+move_ca_files() {
+    local location="$1"
+    echo "Moving CA certificates to CA store location $1"
+    mv -f root_ca.crt int_ca.crt "${location}/"
+}
 
-mv -f root_ca.pem int_ca.pem $INSTALL_DIR/
+# Determine OS distribution
+DISTRO="$(lsb_release -is | tr "[:upper:]" "[:lower:]")"
 
-update-ca-trust
+case $DISTRO in
+ubuntu)
+    INSTALL_DIR="/usr/local/share/ca-certificates"
+    move_ca_files $INSTALL_DIR
+    update-ca-certificates
+    ;;
+centos)
+    INSTALL_DIR="/usr/share/pki/ca-trust-source/anchors"
+    move_ca_files $INSTALL_DIR
+    update-ca-trust
+    ;;
+*)
+    ;;
+esac
