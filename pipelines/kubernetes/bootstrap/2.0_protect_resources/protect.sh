8d4c391c353ed5c320f7a3ae89209e120816f81c
Constantin Bugneac <cosbug@gmail.com>
2024-08-15 18:44:03 +0000

    minor update

--- pipelines/kubernetes/bootstrap/2.0_protect_resources/protect.sh
+++ pipelines/kubernetes/bootstrap/2.0_protect_resources/protect.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-kubectl set resources deploy/coredns -n ${NAMESPACE} --limits='cpu=100m,memory=170Mi' --requests='cpu=100m,memory=170Mi' || true
+kubectl set resources deploy/coredns -n ${NAMESPACE} --requests='cpu=100m,memory=170Mi' --limits='cpu=100m,memory=170Mi' || true
